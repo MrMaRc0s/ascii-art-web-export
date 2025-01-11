0bd90546d9d583a3eae0c2f2f5bf4b77c000cf61
cmarkos <cmarkos@MrMaRc0s.>
2025-01-11 23:40:35 +0200

    +download pdf

--- downloadAsciiArtHandler.go
+++ downloadAsciiArtHandler.go
@@ -10,39 +10,32 @@ func downloadAsciiArtHandler(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-
 	err := r.ParseForm()
 	if err != nil {
 		http.Error(w, "Bad Request - Unable to parse form data", http.StatusBadRequest)
 		return
 	}
-
 	inputText := r.FormValue("text")
 	banner := r.FormValue("banner")
 	if inputText == "" {
 		http.Error(w, "Bad Request - Empty Text", http.StatusBadRequest)
 		return
 	}
-
 	if banner != "standard" && banner != "shadow" && banner != "thinkertoy" {
 		http.Error(w, "Bad Request - Invalid banner template", http.StatusBadRequest)
 		return
 	}
-
 	// Generate ASCII art
 	artResult, err := GenerateAsciiArt(inputText, banner)
 	if err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
-
 	artLength := len(artResult)
-
 	// Serve ASCII art as a downloadable file
 	w.Header().Set("Content-Disposition", "attachment; filename=ascii_art.txt")
 	w.Header().Set("Content-Type", "text/plain")
 	w.Header().Set("Content-Length", strconv.Itoa(artLength))
-
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(artResult))
 }
--- main.go
+++ main.go
@@ -13,37 +13,31 @@ type customMux struct {
 func (c *customMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// List of valid paths
 	validPaths := map[string]bool{
-		"/":                   true,
-		"/ascii-art":          true,
-		"/download-ascii-art": true, //curl -X POST -d "text=text&banner=standard" -i http://localhost:8080/download-ascii-art
+		"/":                       true,
+		"/ascii-art":              true,
+		"/download-ascii-art-pdf": true, //curl -X POST -d "text=text&banner=standard" -i http://localhost:8080/download-ascii-art
+		"/download-ascii-art":     true,
 	}
-
 	// Check if the requested path is valid
 	if !validPaths[r.URL.Path] {
 		http.NotFound(w, r)                           // Return 404 if path is not valid
 		fmt.Printf("404 Not Found: %s\n", r.URL.Path) // Optional: Log the unmatched path for debugging
 		return
 	}
-
 	// Serve the request if the path is valid
 	c.mux.ServeHTTP(w, r)
 }
-
 func main() {
 	mux := http.NewServeMux()
-
 	// Register the main page handler for the root path
 	mux.HandleFunc("/", mainPage)
-
 	// Register the /download-ascii-art handler for the export
+	mux.HandleFunc("/download-ascii-art-pdf", downloadAsciiArtHandlerPdf)
 	mux.HandleFunc("/download-ascii-art", downloadAsciiArtHandler)
-
 	// Register the /ascii-art handler for POST requests
 	mux.HandleFunc("/ascii-art", asciiArtHandler)
-
 	// Wrap the mux in our customMux to handle 404 errors
 	wrappedMux := &customMux{mux: mux}
-
 	port := 8080
 	fmt.Print("Server is running on http://localhost:8080 ")
 	err := http.ListenAndServe(":8080", wrappedMux)
@@ -52,5 +46,4 @@ func main() {
 		fmt.Printf("\rServer is running on http://localhost:%v ", port)
 		err = http.ListenAndServe(fmt.Sprintf(":%d", port), wrappedMux)
 	}
-
 }
